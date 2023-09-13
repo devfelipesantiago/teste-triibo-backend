0065e0329349977997debce74c33473222b4f7bc
felipe santiago <devfelipesantiago>
2023-09-13 15:52:45 -0300

    Feat: Router

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
-import express from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require('./routes');
 
 const app = express();
 
-app.listen(3001, (err, result) => console.log('Service listening on port 3001'));
+app.use(bodyParser.json());
+app.use('/', routes);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, async () => console.log(`Server listening on port ${port}`));
