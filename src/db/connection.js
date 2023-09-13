4f54a29b5eceb895bfb64ecbfd415fb78d58c50b
felipe santiago <devfelipesantiago>
2023-09-13 16:14:35 -0300

    Feat: Docker compose Docker file

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   port: process.env.MYSQL_PORT || 33060,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'shopperTest',
+  database: process.env.DB_NAME || 'triiboMessages',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
